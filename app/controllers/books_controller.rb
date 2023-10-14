d8e33437f63f9fb36315739cb8a1414d4f839ed8
tsubamenoki <144215790+tsubamenoki@users.noreply.github.com>
2023-10-14 17:51:17 +0000

    パスの名前変更から

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,21 +1,47 @@
 class BooksController < ApplicationController
-  def new
-  end
   
   def create
-    book=Book.new(book_params)
-    book.save
-    redirect_to '/top'
+    @book=Book.new(book_params)
+    if @book.save
+      flash[:notice]= "Book was successfully created."
+      redirect_to book_path(@book.id)
+    else
+      @books=Book.all
+      render :index
+    end
   end
   
   def index
+    @books=Book.all
     @book=Book.new
+    
+    #@booksは取得データすべて。@bookは新規作成したインスタンス
+    
   end
   
   def show
+    @book=Book.find(params[:id])
   end
   
   def edit
+    @book=Book.find(params[:id])
+  end
+  
+  def update
+    @book=Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice]= "Book was successfully updated."
+      redirect_to book_path(@book.id)
+    else 
+      render :edit
+    end
+  end
+  
+  def destroy
+    book=Book.find(params[:id])
+    book.destroy
+     flash[:notice]="Book was successfully destroyed."
+    redirect_to books_path
   end
   
   private
